db5b14bd45f4d30bc747131da30a25f4b418007e
Connor S <constroh@gmail.com>
2023-05-04 18:08:06 -0500

    updated Home.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 
 function App() {
-  // let name = "Connor";
-  const [name, setName] = useState("Connor");
-  // Array destructuring
-  const [age, setAge] = useState(23);
-
-  const handleClick = () => {
-    setName("Jesse");
-    setAge(26);
-  };
-
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
-        <p>
-          {name} is {age} years old
-        </p>
-        <button onClick={handleClick}>Click me</button>
       </div>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 const Home = () => {
+  // let name = "Connor";
+  const [name, setName] = useState("Connor");
+  // Array destructuring
+  const [age, setAge] = useState(23);
+
+  const handleClick = () => {
+    setName("Jesse");
+    setAge(26);
+  };
+
   return (
     <div className="home">
-        <h2>Home</h2>
+      <h2>Home</h2>
+      <p>
+        {name} is {age} years old
+      </p>
+      <button onClick={handleClick}>Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
